00d21b0c2c8aef1bf09751d6ae730b1b8cded615
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-05-20 19:26:22 +0200

    feat: added cards on dom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,26 @@
         </div>
     </section>
    
+    <!--Cards-->
+    <section class="cards d-flex flex-wrap mt-5">
+        
+        @foreach ($comics as $comic)
+            <div class="card col-2 p-3">
 
-    <section></section>
+                <div class="d-flex flex-column">
+                    <img src="{{ $comic['thumb'] }}" alt="comics" class="card-img-top">
+                    <h5 class="card-title ms">{{ $comic['title'] }}</h5>
+                </div>
+                
+            </div>
+        @endforeach
+    </section>
+
+    <!--Shop-->
+    <section>
+
+        
+    </section>
+    
     
 @endsection
